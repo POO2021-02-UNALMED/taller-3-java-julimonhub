910fe1ab0ae211623c0f929354cb3b696a83bfb4
julimonhub <92111604+julimonhub@users.noreply.github.com>
2021-11-02 18:49:47 -0500

    nbn

--- src/main/java/taller3/televisores/Marca.java
+++ src/main/java/taller3/televisores/Marca.java
@@ -1,18 +1,18 @@
 package taller3.televisores;
 
-public class Marca{
+public class Marca {
 	
-		private String nombre;
-		
-		public Marca(String nombre) {
-				this.nombre= nombre; 
-		}
-		
-		public void setNombre(String nombre) {
-						this.nombre=nombre;
-		}
-
-		public String getNombre() {
-				return nombre;
-		}
+	private String nombre;
+	
+	public Marca(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public void setNombre(String nombre) {
+			this.nombre = nombre;	
+	}
+	
+	public String getNombre() {
+		return nombre;		
+	}
 }
