ef6aacc65c4c916fc42f9bebf64fa2ee35493ecd
julimonhub <92111604+julimonhub@users.noreply.github.com>
2021-11-02 16:05:20 -0500

    bart

--- src/main/java/taller3/televisores/Control.java
+++ src/main/java/taller3/televisores/Control.java
@@ -1,46 +1,46 @@
 package taller3.televisores;
 
 public class Control{
-	TV tv;
+	    TV tv;
 	
-	pulic void enlazar(TV tv) {
-		this.tv=tv
-		tv.setControl(this);
-	}
+	    pulic void enlazar(TV tv) {
+		       this.tv=tv;
+		       tv.setControl(this);
+	    }
 	
-	public void (setTv){
-		this.tv=tv;
-    }
+	    public void turnOn() {
+		        tv.turnOn();
+        }
 	
-	public TV getTV() {
-		return tv;
-	}
+	    public void turnOff() {
+		        tv.turnOff();
+	    }
 	
-	public void canalup() {
-		tv.canalUp();
-	}
+	    public void canalup() {
+		        tv.canalUp();
+	    }
 	
-	public void canalDown() {
-		tv.canalDown();
-	}
+	    public void canalDown() {
+		        tv.canalDown();
+	    }
 	
-	public void volumenUp() {
-		tv.volumenUp();
-	}
+	    public void volumenUp() {
+		        tv.volumenUp();
+	    }
 	
-	public void volumenDown() {
-		tv.volumenDown();
-	}
+	    public void volumenDown() {
+		        tv.volumenDown();
+	    }
 	
-	public void turnOn() {
-		tv.turnOn();
-	}
+	    public void setCanal(int canal) {
+		        tv.setCanal(canal);
+	    }
 	
-	public void turnOff(){
-		tv.turnOff();
-	}
+	    public void setTv(TV tv) {
+		        this.tv=tv;
+	    }
 	
-	public void setCanal(int canal) {
-		tv.setCanal(canal);
-	}
+	    public TV getTv(){
+		        return tv;
+	    }
 }
