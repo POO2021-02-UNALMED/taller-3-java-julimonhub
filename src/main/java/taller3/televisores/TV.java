dd1087632bf5a0ccfefadb7138847aeceade7f18
julimonhub <92111604+julimonhub@users.noreply.github.com>
2021-11-02 14:40:37 -0500

    control1

---
+++ src/main/java/taller3/televisores/TV.java
@@ -0,0 +1,48 @@
+package taller3.televisores;
+
+public class Control{
+	TV tv;
+	
+	pulic void enlazar(TV tv) {
+		this .tv==tv
+		tv.setControl(this);
+	}
+	
+	public void setTv){
+		this.tv=tv;
+}
+	
+	public TV getTV() {
+		return tv;
+	}
+	
+	public void canalup() {
+		tv.canalup();
+	}
+	
+	public void canalDown() {
+		tv.canalDown();
+	}
+	
+	public void volumenUp() {
+		tv.volumenUp();
+	}
+	
+	public void volumenDown() {
+		tv.volumenDown();
+	}
+	
+	public void turnOn() {
+		tv.turnon();
+	}
+	
+	public void turnOff(){
+		tv.turnOff();
+	}
+	
+	public void setCanal(int canal) {
+		tv.setCanal(canal);
+	}
+}
+	}
+	}
